5808e89b12b8ab4cd24b614b37cf6cae35aef5af
Andreas Hoepfel <reasoriginal@gmail.com>
2024-01-23 07:39:20 +0100

    fixed navbar

--- src/lib/navbar.svelte
+++ src/lib/navbar.svelte
@@ -1,8 +1,9 @@
-<script>
+<script lang="ts">
+    import { base } from '$app/paths';
 </script>
 
 <div class="header flex justify-center items-center ">
-	<a href="/">
+	<a href="{base}/">
 		<div class="text-3xl mb-3 font-bold">picsel.</div> 
 	</a>
 </div>
