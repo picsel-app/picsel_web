c350b6be3add69abfe730756c800cbdcfc036fe0
Andreas Hoepfel <reasoriginal@gmail.com>
2024-01-24 20:40:53 +0100

    added some pictures and text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,35 +1,62 @@
+<div class="min-h-fit bg-white">
+	<div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+		<div class="mx-auto place-self-center lg:col-span-7">
+			<h1
+				class="max-w-2xl mb-4 text-xl font-bold tracking-tight leading-none md:text-2xl xl:text-3xl "
+			>
+				Bilder zu teilen war nie einfacher!
+			</h1>
+			<p
+				class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400"
+			>
+			Halte den Moment fest, teile deine Bilder, deine Geschichten mühelos mit der Welt. 
+			Es ist an der Zeit, deine Fotos die unverfälschte Geschichte erzählen zu lassen - einfach, schnell und nur einen Klick entfernt!
+			</p>
+			<div class="flex flex-wrap justify-center my-10">
+				<a href="https://play.google.com/store">
+					<div class="flex items-center border w-56 rounded-lg px-4 py-2 w-52 mx-2 my-5">
+						<img src="https://cdn-icons-png.flaticon.com/512/888/888857.png" class="w-7 md:w-8" />
+						<div class="text-left ml-3">
+							<p class="text-xs text-black-200">Download on</p>
+							<p class="text-sm md:text-base">Google Play Store</p>
+						</div>
+					</div>
+				</a>
+				<a href="https://apps.apple.com/de/app/weet-planen-treffen/id6448232263">
+					<div class="flex items-center border w-56 rounded-lg px-4 py-2 w-44 my-5">
+						<img src="https://cdn-icons-png.flaticon.com/512/888/888841.png" class="w-7 md:w-8" />
+						<div class="text-left ml-3">
+							<p class="text-xs text-black-200">Download on</p>
+							<p class="text-sm md:text-base">Apple Store</p>
+						</div>
+					</div>
+				</a>
+			</div>
+		</div>
+		<div class="lg:mt-0 lg:col-span-5 lg:flex">
+			<img src="./images/home/ScreenShot_Picsel.png" alt="mockup" />
+		</div>
+	</div>
 
-    <!-- Main Content -->
-    <div class="container mx-auto mt-8">
+	<div class="grid max-w-screen-xl px-2 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+		<div class="lg:mt-0 lg:col-span-7 lg:flex">
+			<img src="./images/home/ScreenShot_Picsel_2.png" alt="mockup" />
+		</div>
+		<div class="mx-auto place-self-center lg:col-span-5 ">
+			<h1
+				class="max-w-2xl mb-4 text-xl font-bold tracking-tight leading-none md:text-2xl xl:text-3xl"
+			>
+				Markiere deine Freunde und finde verlorene Erinnerungen
+			</h1>
+			<p class="max-w-2xl mb-6 font-light lg:mb-8 md:text-lg lg:text-xl">
+				Freunde markieren, Erinnerungen wiederentdecken. 
+				Müheloses teilen, unvergessliche Momente. 
+				Deine Fotos, deine Geschichten - wunderschön verwoben.
+			</p>
+		</div>
+	</div>
 
-        <!-- Product Description Section -->
-        <section class="mb-8">
-            <h2 class="text-3xl font-semibold mb-4">About Our App</h2>
-            <p class="text-gray-700">
-                Your Media Sharing App allows users to effortlessly share their media, whether it's photos, videos, or
-                audio files. Join our community and start sharing your moments today!
-            </p>
-        </section>
-
-        <!-- Features Section -->
-        <section class="mb-8">
-            <h2 class="text-3xl font-semibold mb-4">Key Features</h2>
-            <ul class="list-disc list-inside text-gray-700">
-                <li>Easy media uploading</li>
-                <li>Secure sharing options</li>
-                <li>User-friendly interface</li>
-                <li>Customizable privacy settings</li>
-            </ul>
-        </section>
-
-        <section>
-            <p class="text-gray-700">
-                There will be more on here soon!
-            </p>
-        </section>
-
-    </div>
-
+</div>
   
   <style lang="postcss">
 
